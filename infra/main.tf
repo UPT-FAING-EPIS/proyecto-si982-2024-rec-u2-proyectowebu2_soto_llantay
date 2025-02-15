fd69cd0ca89def6826b700515aa4d0babad2e206
marjioryllantay <marllantay@upt.pe>
2025-02-15 11:40:01 -0500

    l

---
+++ infra/main.tf
@@ -0,0 +1,52 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0.0"
+    }
+  }
+  required_version = ">= 0.14.9"
+}
+
+variable "suscription_id" {
+  type        = string
+  description = "aba87226-03f9-4205-a6d4-a62d7e08e8ab"
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = var.suscription_id
+}
+
+# Crear el grupo de recursos
+resource "azurerm_resource_group" "rg" {
+  name     = "upt-arg-app"
+  location = "centralus"
+}
+
+# Crear el Plan de Servicio de Aplicación (Windows)
+resource "azurerm_service_plan" "appserviceplan" {
+  name                = "upt-asp-app"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  os_type             = "Windows"  # Cambiado a Windows
+  sku_name            = "F1"
+}
+
+# Crear la Aplicación Web en Azure para .NET Framework
+resource "azurerm_windows_web_app" "webapp" {
+  name                = "upt-awa-bolsatrabajo"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  service_plan_id     = azurerm_service_plan.appserviceplan.id
+  depends_on          = [azurerm_service_plan.appserviceplan]
+
+  site_config {
+    minimum_tls_version = "1.2"
+    always_on           = false
+    application_stack {
+      current_stack = "dotnet"
+      dotnet_version = "v4.8"  # Especifica la versión de .NET Framework
+    }
+  }
+}
